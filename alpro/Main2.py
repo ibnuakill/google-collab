68cc42e2e96d9dd12d5d184e76ef90b537ddf9f1
ibnuakill <sarkamganas406.com>
2024-01-16 13:36:54 +0700

    tes

---
+++ alpro/Main2.py
@@ -0,0 +1,29 @@
+while True:
+    jawab = input("Apakah anda sarapan(y/n)")
+    if jawab == 'y': #kondisi false akan menriger perintah break
+        print('semangat!!!')
+        break #perintah menghentikan program
+
+# for hitung_hura in range(1,4):
+#     for hitung_hip in range(1,3):
+#         print('Hip')
+#     print('Hura-Hura')
+
+# buah = ["Apel", "Jeruk", "Anggur"]
+# for makanan in buah:
+#     print("saya suka makan", makanan)
+
+# for hitung_ayu in range(1,3):
+#     for hitung_ting in range(1,2):
+#         print('Ayu')
+#     print('Ting-Ting')
+
+# i = 2
+# while(i < 100):
+#     j = 2
+#     while(j <= (i/j)):
+#         if not(i%j): break
+#         j = j + 1
+#     if (j > i/j) : print (i, "Adalah bilangan prima")
+#     i = i + 1
+# print ("Selesai!")
---
+++ alpro/MainTes.py
@@ -0,0 +1,12 @@
+Algoritma Runtunan_1
+#Program Runtunan_1
+#Contoh program yang menghasilkan keluaran
+#berbeda jika urutan instruksi diubah
+#DEKLARASI
+#A, B, VARIABEL
+#DESKRIPSI
+A=10
+A=2*A
+B=A
+print (B)
+20
